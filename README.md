15acf3335ee0a05a4524a69526093b200135118f
Christian Marienfeld <post@chrisland.de>
2019-10-11 23:22:27 +0200

    init (fork from "Webportal Installer" )

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# SchuleIntern-Backup-Restore
+# SchuleIntern - Backup Restore
+
+
+## How To Use
+
+- Download Repro
+- Move "www/restore" to your Webserver
+- open YOUR_URL/restore
+
+
+
+
+
+## Development
+
+
+### Project init
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run release
+```
+
+### PHP Modification
+
+at ./restore.php
+NOT ./www/restore/restore.php !!!
---
+++ src/components/Server.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="server">
+
+    <div class="outer-box">
+      <div class="boxlist">
+        <h2>Wiederherstellen Ihres SchuleIntern Backups.</h2>
+        <p>
+          Bitte legen Sie sich folgende Daten bereit:
+        </p>
+        <ul class="ullist">
+          <li>SchuleIntern Backupdatei im *.zip Format</li>
+          <li>Zugangsdaten für Ihre MySQL / MariaDB Datenbank</li>
+        </ul>
+      </div>
+      <div class="box box-bg-white">
+          
+        <h3>Server Test</h3>
+
+        <ul>
+
+          <li>
+            <div class="box-long">
+              Schreibrechte im Übergeordnetem Verzeichnis
+              <div class="info">
+                {{values.upperDir}}
+              </div>
+            </div>
+            <div class="box-icon">
+              <img v-if="!loading && values.upperDirWriteable" src="../assets/icons/check.svg" alt="ok" title="ok"/>
+              <img v-else-if="!loading && !values.upperDirWriteable" src="../assets/icons/cancel.svg" alt="error" title="error"/>
+              <img v-if="loading" src="../assets/icons/spinner.gif" alt="loading" title="loading"/>
+            </div>
+          </li>
+          <li>
+            <div class="box-long">
+              PHP Version
+              <div class="info">
+                Min. 7.2 - Installiert: {{values.phpVersion}}
+              </div>
+            </div>
+            <div class="box-icon">
+              <img v-if="!loading && values.phpVersionCompare" src="../assets/icons/check.svg" alt="ok" title="ok"/>
+              <img v-else-if="!loading && values.phpVersionCompare == false" src="../assets/icons/cancel.svg" alt="error" title="error"/>
+              <img v-if="loading" src="../assets/icons/spinner.gif" alt="loading" title="loading"/>
+            </div>
+          </li>
+          <li>
+            <div class="box-long">
+              PHP Zip Erweiterung
+            </div>
+            <div class="box-icon">
+              <img v-if="!loading && values.zipEnable" src="../assets/icons/check.svg" alt="ok" title="ok"/>
+              <img v-else-if="!loading && values.zipEnable == false" src="../assets/icons/cancel.svg" alt="error" title="error"/>
+              <img v-if="loading" src="../assets/icons/spinner.gif" alt="loading" title="loading"/>
+            </div>
+          </li>
+          
+        </ul>
+        <div class="btn-box">
+          <button v-if="btnState == false" @click="refresh"
+            class="btn">Erneut Testen</button>
+        </div>
+
+      </div>
+    </div>
+    <div class="btn-outer-box">
+      <button v-if="btnState" @click="next" class="btn">Weiter</button>
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+
+
+export default {
+  name: 'Server',
+  props: {
+    apiRoot: String
+  },
+  data: function () {
+    return {
+      values: false,
+      btnState: false,
+      loading: false
+    }
+  },
+  created: function () {
+    this.init();
+  },
+  methods: {
+    init: function () {
+
+      var that = this;
+
+      this.loading = true;
+
+      axios.get(this.apiRoot+'restore.php?action=server')
+      .then( function(response) {
+        that.setData(response.data);
+        
+      }).catch(function (error) {
+
+        that.setData( {
+          upperDirWriteable: false,
+          phpVersionCompare: false,
+          phpVersion: false
+        });
+
+        console.error(error);
+      });
+
+    },
+    setData: function (data) {
+
+      this.values = data;
+
+      if ( !this.required() ) {
+          this.btnState = false;
+      } else {
+          this.btnState = true;
+      }
+
+      this.loading = false;
+
+    },
+    required: function () {
+
+      if (!this.values.upperDirWriteable
+         || !this.values.phpVersionCompare
+         || !this.values.phpVersion) {
+          return false;
+      } else {
+          return true;
+      }
+
+    },
+    refresh: function () {
+
+      this.init();
+
+    },
+    next: function () {
+
+      if ( !this.required() ) {
+          return false;
+      }
+
+      EventBus.$emit('done--step', {
+        server: true
+      })
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
